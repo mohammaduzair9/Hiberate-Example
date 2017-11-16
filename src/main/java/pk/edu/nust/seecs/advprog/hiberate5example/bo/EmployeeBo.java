a6a2e0f1409ead5748755ea261506229e8243fb8
mohammaduzair9 <31093445+mohammaduzair9@users.noreply.github.com>
2017-11-16 14:18:20 +0500

    Code Cleanup

--- src/main/java/pk/edu/nust/seecs/advprog/hiberate5example/bo/EmployeeBo.java
+++ src/main/java/pk/edu/nust/seecs/advprog/hiberate5example/bo/EmployeeBo.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pk.edu.nust.seecs.advprog.hiberate5example.bo;
 
-/**
- *
- * @author Fahad Satti <fahad.satti@seecs.edu.pk>
- */
 public interface EmployeeBo {
 
     int addContractEmployee(int empIndex, float pay_per_hour, String contract_period);
